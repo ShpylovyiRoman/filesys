31e71def61d2713ed28b0ccd48d55992d4c4a667
ShpylovyiRoman <rendonon@gmail.com>
2021-11-16 22:42:28 +0200

    Add user managment

--- src/users.rs
+++ src/users.rs
@@ -1,9 +1,20 @@
 use std::collections::HashMap;
 
+use anyhow::anyhow;
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Argon2,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct UserId(u64);
+
+impl Default for UserId {
+    fn default() -> Self {
+        ADMIN_ID
+    }
+}
 
 impl UserId {
     pub fn new(num: u64) -> Self {
@@ -63,6 +74,39 @@ pub struct User {
     pass: String,
 }
 
+impl User {
+    pub fn new(id: UserId, name: Username, pass: &str) -> anyhow::Result<Self> {
+        let mut this = Self {
+            id,
+            name,
+            pass: Default::default(),
+        };
+        this.change_pass(pass)?;
+        Ok(this)
+    }
+
+    pub fn verify_pass(&self, pass: &str) -> anyhow::Result<Option<()>> {
+        let parsed_hash =
+            PasswordHash::new(&self.pass).map_err(|err| anyhow!("parsing hash: {}", err))?;
+        let argon2 = Argon2::default();
+        Ok(argon2.verify_password(pass.as_bytes(), &parsed_hash).ok())
+    }
+
+    pub fn change_pass(&mut self, pass: &str) -> anyhow::Result<()> {
+        let salt = SaltString::generate(rand::thread_rng());
+
+        let argon2 = Argon2::default();
+
+        let pass = argon2
+            .hash_password(pass.as_bytes(), &salt)
+            .map_err(|err| anyhow!("deriving hash: {}", err))?
+            .to_string();
+
+        self.pass = pass;
+        Ok(())
+    }
+}
+
 impl Perms {
     pub fn intersects(&self, ops: &[Op]) -> bool {
         ops.iter()
@@ -105,5 +149,62 @@ impl AccessMap {
 
     pub fn get(&self, uid: UserId) -> Perms {
         self.perms.get(&uid).copied().unwrap_or_default()
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct UserDb {
+    id_ctr: UserId,
+    unames: HashMap<Username, UserId>,
+    users: HashMap<UserId, User>,
+}
+
+fn wrong_uname() -> anyhow::Error {
+    anyhow!("wrong username or password")
+}
+
+impl UserDb {
+    pub fn new() -> anyhow::Result<Self> {
+        let mut this = Self::default();
+        this.add_user("admin", "")?;
+        Ok(this)
+    }
+
+    pub fn add_user(&mut self, name: &str, pass: &str) -> anyhow::Result<UserId> {
+        if self.unames.contains_key(name) {
+            anyhow::bail!("user exists")
+        }
+        let id = self.id_ctr.tick_next();
+
+        let user = User::new(id, name.to_string(), pass)?;
+        self.unames.insert(name.to_string(), id);
+        self.users.insert(id, user);
+
+        Ok(id)
+    }
+
+    pub fn login(&self, name: &str, pass: &str) -> anyhow::Result<UserId> {
+        let uid = self.unames.get(name).ok_or_else(wrong_uname)?;
+        self.login_with_id(*uid, pass).map(|_| *uid)
+    }
+
+    pub fn login_with_id(&self, uid: UserId, pass: &str) -> anyhow::Result<()> {
+        let user = self.users.get(&uid).ok_or_else(wrong_uname)?;
+        user.verify_pass(pass)?.ok_or_else(wrong_uname)
+    }
+
+    pub fn change_pass(
+        &mut self,
+        uid: UserId,
+        old_pass: &str,
+        new_pass: &str,
+    ) -> anyhow::Result<()> {
+        self.login_with_id(uid, old_pass)?;
+        let user = self
+            .users
+            .get_mut(&uid)
+            .expect("already checked for existence");
+
+        user.change_pass(new_pass)
     }
 }
